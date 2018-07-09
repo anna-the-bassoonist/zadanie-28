8ff704291721a196dcda1e9c0bbe8bf709f17907
Anna Toeplitz <anafagocistka@gmail.com>
2018-07-10 01:00:29 +0200

    zadanie28

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var text = "moja liczba";
 console.log(text);
 var number = 12;
 console.log(number);
-console.log(text+' równa się '+number);
+
 if (number > 10) {
     console.log("Podana liczba jest większa od 10");
 } else{
