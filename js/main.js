5f354aa2feb9d829530f63df929a5b0cc3cc78f2
Anna Toeplitz <anafagocistka@gmail.com>
2018-07-09 17:00:57 +0200

    pierwsze próby

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var text = "moja liczba";
+console.log(text);
+var number = 12;
+console.log(number);
+console.log(text+' równa się '+number);
+if (number > 10) {
+    console.log("Podana liczba jest większa od 10");
+} else{
+    console.log("Podana liczba nie jest większa od 10");
+}
